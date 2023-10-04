c6c0236817496f362ad87ddea1b82725f6662bfe
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-10-04 21:57:12 +0200

    edit e delete strumenti

--- src/main/java/com/projectwork/shopstrumentimusicali/shop/controller/StrumentoController.java
+++ src/main/java/com/projectwork/shopstrumentimusicali/shop/controller/StrumentoController.java
@@ -8,16 +8,16 @@ import com.projectwork.shopstrumentimusicali.shop.repository.StrumentoRepository
 import com.projectwork.shopstrumentimusicali.shop.repository.TipologiaRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin/strumenti")
@@ -65,6 +65,60 @@ public class StrumentoController {
         return "redirect:/admin/strumenti";
     }
     // modifica strumento
+    @GetMapping("/{slug}/edit")
+    public String edit(Model model , @PathVariable("slug") String slug){
+        // prendo l optional ny slug
+        Optional<Strumento> result= strumentoRepository.findBySlug(slug);
+        // controllo dell optional
+        if (result.isPresent()){
+            model.addAttribute("strumento",result.get());
+            return "admin/strumenti/form"; // template
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "strumento con slug " + slug + " non trovata");
+
+        }
+
+
+
+    }
+
+    @PostMapping("/{slug}/edit")
+    public String doEdit(@PathVariable("slug") String slug,@Valid @ModelAttribute("strumento") Strumento formStrumento,
+                           BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+
+            return "admin/strumenti/form";
+        }
+        // trovo la tipologia
+        Strumento strumento= strumentoRepository.findBySlug(slug).get();
+        formStrumento.setTipologia(strumento.getTipologia());
+
+        // salvo lo slug nell formStrumento
+        formStrumento.setSlug(Utils.getSlug(formStrumento.getNome()));
+        formStrumento.setId(strumento.getId());
+        System.out.println(formStrumento.getTipologia().getNome());
+        // salvo nel db
+        strumentoRepository.save(formStrumento);
+        return "redirect:/admin/strumenti";
+    }
     // elimina strumeno
+    @PostMapping("/{slug}/delete")
+    public String delete(@PathVariable("slug") String slug) {
+        // legge  strumento dal path
+        Optional<Strumento> result= strumentoRepository.findBySlug(slug);
+        if (result.isPresent()){
+            int userId= result.get().getId();
+            // cancella lo User da database
+            strumentoRepository.deleteById(userId);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "strumento con slug " + slug + " non trovata");
+
+        }
+
+        // fa una redirect alla lista di User
+        return "redirect:/admin/strumenti";
+    }
 
 }
--- src/main/java/com/projectwork/shopstrumentimusicali/shop/repository/StrumentoRepository.java
+++ src/main/java/com/projectwork/shopstrumentimusicali/shop/repository/StrumentoRepository.java
@@ -3,5 +3,8 @@ package com.projectwork.shopstrumentimusicali.shop.repository;
 import com.projectwork.shopstrumentimusicali.shop.model.Strumento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StrumentoRepository extends JpaRepository<Strumento,Integer>  {
+    Optional<Strumento> findBySlug(String slug);
 }
