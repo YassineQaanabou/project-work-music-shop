eddcbc7dfd515ebc121a81f099571e77db6c7686
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-10-05 16:32:52 +0200

    fragment navbar

--- src/main/java/com/projectwork/shopstrumentimusicali/shop/controller/IndexController.java
+++ src/main/java/com/projectwork/shopstrumentimusicali/shop/controller/IndexController.java
@@ -1,15 +1,21 @@
 package com.projectwork.shopstrumentimusicali.shop.controller;
 
+import com.projectwork.shopstrumentimusicali.shop.repository.TipologiaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    @Autowired
+    private TipologiaRepository tipologiaRepository;
     // homepage
     @GetMapping
-    public  String homepage(){
+    public  String homepage(Model model){
+        model.addAttribute("tipologie",tipologiaRepository.findAll());
         return "homepage";
     }
     // {nome categoria}
