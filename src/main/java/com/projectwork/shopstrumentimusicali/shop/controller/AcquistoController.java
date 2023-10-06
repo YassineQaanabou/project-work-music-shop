c7bdc6718ba0361cfa5a9e6f673fa16209258a88
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-10-06 18:26:49 +0200

    acquisto controller

--- src/main/java/com/projectwork/shopstrumentimusicali/shop/controller/AcquistoController.java
+++ src/main/java/com/projectwork/shopstrumentimusicali/shop/controller/AcquistoController.java
@@ -1,6 +1,10 @@
 package com.projectwork.shopstrumentimusicali.shop.controller;
 
+import com.projectwork.shopstrumentimusicali.shop.model.Acquisto;
+import com.projectwork.shopstrumentimusicali.shop.repository.StrumentoRepository;
+import com.projectwork.shopstrumentimusicali.shop.repository.TipologiaRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,12 +13,21 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/")
 public class AcquistoController {
-
+     @Autowired
+     private StrumentoRepository strumentoRepository;
+     @Autowired
+     private TipologiaRepository tipologiaRepository;
     @GetMapping("/{slugTipo}/{slugStrum}/acquista")
 
     public String create(Model model, @PathVariable("slugTipo") String slugTipo,@PathVariable("slugStrum") String slugStrum){
-        //passo un acquisto vuoto
-        return null;
+        //prendo strumento e tipo
+        model.addAttribute("strumento",strumentoRepository.findBySlug(slugStrum).get());
+        model.addAttribute("tipologia",tipologiaRepository.findBySlug(slugTipo).get());
+         //passo un acquisto vuoto
+        Acquisto acquisto = new Acquisto();
+        acquisto.setQuantità(1);
+        model.addAttribute("acquisto",acquisto);
+        return "customer/acquisti/form";
     }
     @PostMapping("/{slugTipo}/{slugStrum}/acquista")
 
@@ -24,10 +37,15 @@ public class AcquistoController {
                            BindingResult bindingResult,
                            @Valid @ModelAttribute("acquisto") Acquisto acquistoForm
     ){
-        //prendo utente e lo setto nell' acquisto
-        // prendo lo strumento e lo setto nell' acquisto
+        //prendo strumento e tipo
+        // form da cui prendo email dati utente e quiantita
+        // creu un utente con l email
+        // setto l utente appena creato (find by email) nell acquistoform
+        // setto lo strumento nell acquisto form
+        // setto la quantita
 
-        return null;
+
+        return "customer/acquisti/form";
     }
 
 
