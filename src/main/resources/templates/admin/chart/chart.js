fac4481f8cfa663810105dd70c53ae3046fe7d24
Jam Timbol <jam.timbol00@gmail.com>
2023-10-06 18:39:52 +0200

    admin-fixing-chart

---
+++ src/main/resources/templates/admin/chart/chart.js
@@ -0,0 +1,37 @@
+// Define data for your chart
+var data = {
+    labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'],
+    datasets: [{
+        label: 'Sample Data',
+        data: [12, 19, 3, 5, 2],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+
+// Configure and create the chart
+var ctx = document.getElementById('chart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
