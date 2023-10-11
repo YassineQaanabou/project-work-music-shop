60f380593bba62b9ec1440c5dd4319852393183f
YassineQaanabou <qaanabou@gmail.com>
2023-10-11 13:26:24 +0200

    create le crud del fornitore e della tabella ponte fornitore_strumento, creati relativi template e modificato AsortimentoController

--- src/main/java/com/projectwork/shopstrumentimusicali/shop/model/Fornitore.java
+++ src/main/java/com/projectwork/shopstrumentimusicali/shop/model/Fornitore.java
@@ -1,6 +1,7 @@
 package com.projectwork.shopstrumentimusicali.shop.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
@@ -11,11 +12,13 @@ public class Fornitore {
     private Integer id;
     private String nome;
 
-    @OneToMany(mappedBy = "fornitore")
+    @OneToMany(mappedBy = "fornitore", cascade = CascadeType.ALL)
     private List<FornitoreStrumento> fornitoreStrumento;
 
     @OneToMany(mappedBy = "fornitore")
     private List<Assortimento> assortimenti;
+    @Size(max = 100)
+    private String slug;
 
     public Integer getId() {
         return id;
@@ -33,6 +36,10 @@ public class Fornitore {
         return assortimenti;
     }
 
+    public String getSlug() {
+        return slug;
+    }
+
     public void setId(Integer id) {
         this.id = id;
     }
@@ -47,5 +54,9 @@ public class Fornitore {
 
     public void setAssortimenti(List<Assortimento> assortimenti) {
         this.assortimenti = assortimenti;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
     }
 }
