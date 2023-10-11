df1f1f144f96cbf988cc0ba6848b49447c8bbc59
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-10-11 22:09:42 +0200

    creazione service e last month queries... see how to implement

--- src/main/java/com/projectwork/shopstrumentimusicali/shop/api/AcquistoRestController.java
+++ src/main/java/com/projectwork/shopstrumentimusicali/shop/api/AcquistoRestController.java
@@ -4,6 +4,7 @@ import com.projectwork.shopstrumentimusicali.shop.dto.TipologiaVenditaDto;
 import com.projectwork.shopstrumentimusicali.shop.model.Acquisto;
 import com.projectwork.shopstrumentimusicali.shop.model.Tipologia;
 import com.projectwork.shopstrumentimusicali.shop.repository.AcquistoRepository;
+import com.projectwork.shopstrumentimusicali.shop.service.AcquistoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,27 +18,16 @@ import java.util.List;
 public class AcquistoRestController {
     @Autowired
     private AcquistoRepository acquistoRepository;
+    @Autowired
+    private AcquistoService acquistoService;
     @GetMapping("/acquisti")
     public List<Object[]> scquistiList(){
-        List<Object[]> acquisti = acquistoRepository.findVenditeTotaliPerProdotto();
-        return acquisti;
+        return acquistoService.findVenditeTotaliPerProdotto();
     }
 
     @GetMapping("/acquisti-per-tipologia")
     public List<TipologiaVenditaDto> acquistiTimeList(){
-        List<Object[]> risultati = acquistoRepository.findAcquistiPerTipologia();
-
-        // Mappa i risultati nella lista di oggetti TipologiaVenditaDTO
-        List<TipologiaVenditaDto> risultatiDTO = new ArrayList<>();
-        for (Object[] riga : risultati) {
-            Tipologia tipologia = (Tipologia) riga[0];
-            String nomeTipologia = tipologia.getNome();
-            int quantita = ((Long) riga[1]).intValue();
-            TipologiaVenditaDto dto = new TipologiaVenditaDto(nomeTipologia, quantita);
-            risultatiDTO.add(dto);
-        }
-
-        return risultatiDTO;
+        return acquistoService.findAcquistiPerTipologia();
     }
 
         // profitto e
