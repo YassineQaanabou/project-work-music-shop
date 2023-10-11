5eb93188b059c846bfbeedc90ac99b7db67e6f8f
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-10-11 10:46:51 +0200

    dashbard

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,6 +3,9 @@ INSERT INTO tipologia (nome,slug,url_foto) VALUES('chitarre','chitarre','https:/
 INSERT INTO tipologia (nome,slug,url_foto) VALUES('sassofoni','sassofoni','https://m.media-amazon.com/images/I/71S4Ef7BQpL.jpg');
 INSERT INTO tipologia (nome,slug,url_foto) VALUES('batterie','batterie','https://www.tuttomusicasrl.it/wp-content/uploads/2022/05/BATTERIA-ACUSTICA-SOUNDSATION-JDK100-BL-JUNIOR-PER-BAMBINI-2.png');
 
+
+
+
 INSERT INTO strumento (tipologia_id, nome, url_foto, descrizione, prezzo, slug) VALUES (1, 'Yamaha ARIUS YDP-145 Digital Piano', 'https://m.media-amazon.com/images/I/61nxpt3BpdL._AC_SL1500_.jpg', 'Pianoforte Digitale da Casa per Dilettanti, Design Classico ed Elegante, Suonabilità Autentica del Pianoforte Acustico, Bianco', 912.99, 'yamaha-arius-ydp-145-digital-piano');
 INSERT INTO strumento (tipologia_id, nome, url_foto, descrizione, prezzo, slug) VALUES (1, 'Donner Professionale Pianoforte', 'https://m.media-amazon.com/images/I/61sp7SOrwAL._AC_SL1485_.jpg', 'Digitale 88 Tasti Pesati con Supporto per Pianoforte e Pedale, Full Weighted Piano Elettroniche Fullsize per Principianti, DEP-20S', 529.00, 'donner-professionale-pianoforte');
 INSERT INTO strumento (tipologia_id, nome, url_foto, descrizione, prezzo, slug) VALUES (1, 'Medeli Grand 510 BK', 'https://m.media-amazon.com/images/I/51b7QNffLAL._AC_SL1200_.jpg', 'Pianoforte Digitale a Coda', 3049.00, 'medeli-grand-510-bk');
